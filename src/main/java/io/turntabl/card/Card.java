2ee32099dd8377629df2133c6f3f7e94399aaa25
Gerald Addo-Tetteh <gerald.addo-tetteh@turntabl.io>
2023-04-03 11:33:03 +0000

    initial commit

---
+++ src/main/java/io/turntabl/card/Card.java
@@ -0,0 +1,14 @@
+package io.turntabl.card;
+public class Card {
+    private final CardSuit suit;
+    private final CardValue value;
+
+    public Card(CardSuit suit, CardValue value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value.getValue();
+    }
+}
